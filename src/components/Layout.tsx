5cf666f306d457cffc7ac3536d75c59bf2b3cc2a
Ryan Junge <48016417+Ryguy1995@users.noreply.github.com>
2026-09-04 12:04:53 -0700

    updated Layout.tsx and Home.tsx
    updated global styling

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { Footer } from './Footer'
 
 export function Layout() {
   return (
-    <div className="flex min-h-screen flex-col bg-emerald-50 dark:bg-gray-500">
+    <div className="flex min-h-screen flex-col bg-sky-50 dark:bg-sky-900">
       <Navbar />
       <main className="mx-auto w-full max-w-5xl flex-1 px-6 py-12">
         <Outlet />
